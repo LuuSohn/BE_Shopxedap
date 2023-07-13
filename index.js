fd2ea1183f22ab12508ace4b9edef6ee788a46af
LuuSohn <luuhoangson2002@gmail.com>
2023-07-13 23:58:02 +0700

    1.Base code và kết nối mongoose

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/config/database.js";
+
+const app = express();
+const port = process.env.PORT || 8080;
+dotenv.config();
+
+// connect database
+connectDB(process.env.MONGO_URL || "");
+
+// middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
