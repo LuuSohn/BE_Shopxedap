f9ddfea0c439324f273d52d33593693db95ceb53
Namykuuu <128971809+vchien2003@users.noreply.github.com>
2023-07-14 16:03:53 +0700

    [BE] CRUD category

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./src/config/database.js";
-
+import cateRouter from "./src/routers/category.js";
 const app = express();
 const port = process.env.PORT || 8080;
 dotenv.config();
@@ -13,9 +13,10 @@ connectDB(process.env.MONGO_URL || "");
 
 // middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan("shopxedap"));
 app.use(cors());
 
+app.use("/api", cateRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
