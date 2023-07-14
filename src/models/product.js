7f7bbf50e3a6dbad2c973c8889cd17102f502204
huysnake <105479758+huysnake@users.noreply.github.com>
2023-07-14 16:55:28 +0700

    [BE] CRUD product

---
+++ src/models/product.js
@@ -0,0 +1,26 @@
+import { Schema, model, Types } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+const Product = new Schema(
+  {
+    name: { type: String, required: true, text: true },
+    price: { type: Number, required: true, min: 0 },
+    image: { type: Object, required: true },
+    description: { type: String, required: true },
+    categoryId: { type: Types.ObjectId, ref: "Category", required: true },
+    comments: [
+      {
+        userId: { type: Types.ObjectId, ref: "User" },
+        description: String,
+      },
+    ],
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+Product.index({ name: "text" }); // đăng ký chỉ mục văn bản
+Product.plugin(mongoosePaginate);
+
+export default model("Product", Product);
