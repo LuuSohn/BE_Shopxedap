0b0c71b314d79a7298d60a2f2bdb6f89de39fe11
LuuSohn <luuhoangson2002@gmail.com>
2023-07-18 17:35:03 +0700

    Fix Bug image

--- src/models/product.js
+++ src/models/product.js
@@ -5,7 +5,7 @@ const Product = new Schema(
   {
     name: { type: String, required: true, text: true },
     price: { type: Number, required: true, min: 0 },
-    image: { type: Object, required: true },
+    image: { type: String, required: true },
     description: { type: String, required: true },
     categoryId: { type: Types.ObjectId, ref: "Category", required: true },
     comments: [
